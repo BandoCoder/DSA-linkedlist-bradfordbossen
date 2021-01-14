3f5ff878b04d4adf2c9ffffe9746c104de192c51
kittybagpipes <sparticus978@gmail.com>
2021-01-14 08:55:07 -0800

    linkedlist

---
+++ linkedlist.js
@@ -0,0 +1,60 @@
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  inFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+
+  inLast(item) {
+    if (this.head === null) {
+      this.inFirst(item);
+    } else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
+    }
+  }
+
+  find(item) {
+    let currNode = this.head;
+    if (!this.head) {
+      return null;
+    }
+    while (currNode.value !== item) {
+      if (currNode.next === null) {
+        return null;
+      } else {
+        currNode = currNode.next;
+      }
+    }
+    return currNode;
+  }
+
+  remove(item) {
+    if (!this.head) {
+      return null;
+    }
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+    let currNode = this.head;
+    let previousNode = this.head;
+
+    while (currNode != null && currNode.value !== item) {
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log("Item not found");
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
+}
+
+function main() {}
